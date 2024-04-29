1f4a34f121d7979c00d66e1b0d7163089c089d8c
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-04-29 17:43:49 +0530

    implement, use api hook in posts

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,33 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { Container, ListGroup } from 'react-bootstrap';
+import React, { useEffect } from "react";
+import { Container, ListGroup } from "react-bootstrap";
+import useApi from "../hooks/useApi";
 
 const Posts = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const { data: posts, getData } = useApi<Post[]>(); // Utilize the useApi hook
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-        const data: Post[] = await response.json();
-        setPosts(data);
+        await getData("https://jsonplaceholder.typicode.com/posts"); // Use the getData function from the useApi hook
       } catch (error) {
-        console.error('Error fetching posts:', error);
+        console.error("Error fetching posts:", error);
       }
     };
 
     fetchPosts();
-  }, []);
+  }, [getData]);
 
   return (
     <Container>
       <h3 className="mt-3">Recent Posts:</h3>
-      <ListGroup className="mt-3" style={{ maxHeight: '600px', overflowY: 'auto' }}>
-        {posts.map(post => (
-          <ListGroup.Item key={post.id}>
-            <strong>{post.title}</strong>
-            <p>{post.body}</p>
-          </ListGroup.Item>
-        ))}
+      <ListGroup
+        className="mt-3"
+        style={{ maxHeight: "600px", overflowY: "auto" }}
+      >
+        {posts &&
+          posts.map((post) => (
+            <ListGroup.Item key={post.id}>
+              <strong>{post.title}</strong>
+              <p>{post.body}</p>
+            </ListGroup.Item>
+          ))}
       </ListGroup>
     </Container>
   );
